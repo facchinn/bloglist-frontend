ee42b1c36c20699fd5be10239c6bbc487c1c4432
facchinn <nazafacchin@hotmail.com>
2026-08-20 17:09:01 -0300

    update e2e helpers for routed views

--- e2e/tests/helper.js
+++ e2e/tests/helper.js
@@ -1,20 +1,18 @@
 const loginWith = async (page, username, password) => {
+  await page.goto('/login')
   await page.getByLabel('username').fill(username)
   await page.getByLabel('password').fill(password)
   await page.getByRole('button', { name: 'login' }).click()
 }
 
 const createBlog = async (page, { title, author, url }) => {
-  await page.getByRole('button', { name: 'create new blog' }).click()
+  await page.getByRole('link', { name: 'new blog' }).click()
   await page.locator('input[name="title"]').fill(title)
   await page.locator('input[name="author"]').fill(author)
   await page.locator('input[name="url"]').fill(url)
   await page.getByRole('button', { name: 'create' }).click()
 
-  await page
-    .locator('.blog')
-    .filter({ hasText: title })
-    .waitFor()
+  await page.locator('.blog').filter({ hasText: title }).waitFor()
 }
 
 module.exports = { loginWith, createBlog }
