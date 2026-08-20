98b53f7a9e152be0235bbe5517585bbcecc91cda
facchinn <nazafacchin@hotmail.com>
2026-08-20 17:08:32 -0300

    style notifications

--- src/components/Notification.jsx
+++ src/components/Notification.jsx
@@ -1,13 +1,20 @@
+import styled from 'styled-components'
+
+const Message = styled.div`
+  padding: 12px 14px;
+  margin-bottom: 18px;
+  border: 1px solid #888888;
+  border-radius: 7px;
+  background: #f4f4f4;
+  font-weight: 600;
+`
+
 const Notification = ({ message }) => {
   if (message === null) {
     return null
   }
 
-  return (
-    <div>
-      {message}
-    </div>
-  )
+  return <Message>{message}</Message>
 }
 
 export default Notification
