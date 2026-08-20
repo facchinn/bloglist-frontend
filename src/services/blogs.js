caf984733bf98d5a3350d1c25133c032f8008f22
facchinn <nazafacchin@hotmail.com>
2026-08-20 16:48:54 -0300

    add blog update and remove services

--- src/services/blogs.js
+++ src/services/blogs.js
@@ -21,4 +21,17 @@ const create = async newObject => {
   return response.data
 }
 
-export default { getAll, create, setToken }
+const update = async (id, newObject) => {
+  const response = await axios.put(`${baseUrl}/${id}`, newObject)
+  return response.data
+}
+
+const remove = async id => {
+  const config = {
+    headers: { Authorization: token }
+  }
+
+  await axios.delete(`${baseUrl}/${id}`, config)
+}
+
+export default { getAll, create, update, remove, setToken }
